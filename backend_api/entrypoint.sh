15373bdb51be8a197a7aa04dc2c6ebf50b6c9d9d
obinna <obensunday@gmail.com>
2025-03-20 18:42:15 +0100

    fix docker-compose and dockerfile

--- backend_api/entrypoint.sh
+++ backend_api/entrypoint.sh
@@ -2,21 +2,13 @@
 
 if [ "$DATABASE" = "postgres" ]
 then
-    # Check if the database is up and running
     echo "Checking if the database is up and running..."
     while ! nc -z $SQL_HOST $SQL_PORT; do
-    sleep 0.1
+        sleep 0.1
     done
-
     echo "Database is up and running! :-D"
-
 fi
 
-#python manage.py makemigrations
 python manage.py migrate
 
 exec "$@"
-
-
-
-
